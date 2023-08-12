6eeed7bf867ee4dddf13fe9676af4211b2134be4
Narendranath <naren.7090@outlook.com>
2023-08-12 15:00:44 +1000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,63 @@ function App() {
     FormInputType: "text",
     Name: "txtUserName",
   };
+
+  let password: IFormInput = {
+    Lable: "Password",
+    Value: "",
+    PlaceHolder: "Password",
+    FormInputType: "password",
+    Name: "txtPassword",
+  };
+
+  let dob: IFormInput = {
+    Lable: "Date Of birth",
+    Value: "",
+    PlaceHolder: "DD/MM/YYYY",
+    FormInputType: "date",
+    Name: "txtdob",
+  };
+
+  let uemail: IFormInput = {
+    Lable: "E-Mail",
+    Value: "",
+    PlaceHolder: "E-Mail",
+    FormInputType: "email",
+    Name: "txtdob",
+  };
+
   return (
-    <FormInput
-      FormInputType={username.FormInputType}
-      Name={username.Name}
-      Lable={username.Lable}
-      Value={username.Value}
-      PlaceHolder={username.PlaceHolder}
-    />
+    <>
+      <FormInput
+        FormInputType={username.FormInputType}
+        Name={username.Name}
+        Lable={username.Lable}
+        Value={username.Value}
+        PlaceHolder={username.PlaceHolder}
+      />
+      <FormInput
+        FormInputType={password.FormInputType}
+        Name={password.Name}
+        Lable={password.Lable}
+        Value={password.Value}
+        PlaceHolder={password.PlaceHolder}
+      />
+
+      <FormInput
+        FormInputType={dob.FormInputType}
+        Name={dob.Name}
+        Lable={dob.Lable}
+        Value={dob.Value}
+        PlaceHolder={dob.PlaceHolder}
+      />
+      <FormInput
+        FormInputType={uemail.FormInputType}
+        Name={uemail.Name}
+        Lable={uemail.Lable}
+        Value={uemail.Value}
+        PlaceHolder={uemail.PlaceHolder}
+      />
+    </>
   );
 }
 
